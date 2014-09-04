c939cd07bdf4ae29db7680d9e56ec9481193085e
zhili1208 <zhili128@outlook.com>
2014-09-04 15:00:34 -0700

    alert change

--- NuGet.Services.Dashboard.Operations/Tasks/DashBoardTasks/WorkServiceTasks/CreateWorkJobDetailReportTask.cs
+++ NuGet.Services.Dashboard.Operations/Tasks/DashBoardTasks/WorkServiceTasks/CreateWorkJobDetailReportTask.cs
@@ -37,9 +37,11 @@ namespace NuGetGallery.Operations.Tasks.DashBoardTasks
         [Option("CertificateName", AltName = "cername")]
         public string CertificateName { get; set; }
 
+        [Option("lastNhour", AltName = "n")]
+        public int lastNhour { get;set; }
+
         public override void ExecuteCommand()
         {
-            int lastNhour = 24;
             List<WorkInstanceDetail> jobDetail = new List<WorkInstanceDetail>();
             List<WorkJobInstanceDetails> instanceDetails = getWorkjobInstance();
            
@@ -66,26 +68,9 @@ namespace NuGetGallery.Operations.Tasks.DashBoardTasks
                 using (var reader = new StreamReader(respose.GetResponseStream()))
                 {
                     JavaScriptSerializer js = new JavaScriptSerializer();
+                    js.MaxJsonLength = Int32.MaxValue;
                     var objects = js.Deserialize<List<WorkJobInvocation>>(reader.ReadToEnd());
-                    WorkJobInvocation lastJob;
-                    string lastCompleted = string.Empty;
-                    if (objects.Any((item => item.status.Equals("Executed") && item.result.Equals("Completed"))))
-                    {
-                        lastJob = objects.Where(item => item.status.Equals("Executed") && item.result.Equals("Completed")).ToList().FirstOrDefault();
-                    }
-                    else
-                    {
-                        lastJob = objects.FirstOrDefault();
-                    }
-
-                   if (lastJob != null)
-                    {
-                        lastCompleted = string.Format("{0} mins ago", Convert.ToInt32(DateTime.Now.Subtract(lastJob.completedAt).TotalMinutes));
-                    }
-                    else
-                    {
-                        lastCompleted = "N/A";
-                    }
+                    //get last job invocation
 
                     foreach (WorkJobInvocation each in objects)
                     {
@@ -116,57 +101,96 @@ namespace NuGetGallery.Operations.Tasks.DashBoardTasks
                         faultRate = (faultCount * 100 / invocationCount);
                         runtime = ((int)(totalRunTime / invocationCount));
                     }
-                    jobDetail.Add(new WorkInstanceDetail(job.JobInstanceName, job.FrequencyInMinutes+ "mins",lastCompleted , runtime.ToString() + "s", invocationCount.ToString(), faultCount.ToString(), faultRate, ErrorList));
-                    AlertThresholds thresholdValues = new JavaScriptSerializer().Deserialize<AlertThresholds>(ReportHelpers.Load(StorageAccount, "Configuration.AlertThresholds.json", ContainerName));
-                    string[] Igonored = new JavaScriptSerializer().Deserialize<string[]>(ReportHelpers.Load(StorageAccount, "Configuration.WorkerJobToBeIgnored.json", ContainerName));
-                    if (Igonored.Contains(job.JobInstanceName, StringComparer.OrdinalIgnoreCase)) continue;
-                    if (faultRate > thresholdValues.WorkJobErrorThreshold)
+
+                   
+                    WorkJobInvocation lastJob;
+                    string lastCompleted = string.Empty;
+                    if (objects.Any((item => item.status.Equals("Executed") && item.result.Equals("Completed"))))
                     {
-                        new SendAlertMailTask
-                        {
-                            AlertSubject = string.Format("Error: Alert for work job service : {0} failure", job.JobInstanceName),
-                            Details = string.Format("Rate of failure exceeded Error threshold for {0}. Threshold count : {1}%, failure in last 24 hour : {2}", job.JobInstanceName,thresholdValues.WorkJobErrorThreshold , faultCount),
-                            AlertName = string.Format("Error: Work job service {0}", job.JobInstanceName),
-                            Component = "work job service",
-                            Level = "Error"
-                        }.ExecuteCommand();
+                        lastJob = objects.Where(item => item.status.Equals("Executed") && item.result.Equals("Completed")).ToList().FirstOrDefault();
                     }
-                    else if (faultRate > thresholdValues.WorkJobWarningThreshold)
+                    else
                     {
-                        new SendAlertMailTask
-                        {
-                            AlertSubject = string.Format("Warning: Alert for work job service: {0} failure", job.JobInstanceName),
-                            Details = string.Format("Rate of failure exceeded Warning threshold for {0}. Threshold count : {1}%, failure in last 24 hour : {2}", job.JobInstanceName, thresholdValues.WorkJobWarningThreshold, faultCount),
-                            AlertName = string.Format("Warning: Work job service {0}", job.JobInstanceName),
-                            Component = "work job service",
-                            Level = "Warning"
-                        }.ExecuteCommand();
+                        lastJob = objects.FirstOrDefault();
                     }
 
-                    if (invocationCount < ((lastNhour*60 / job.FrequencyInMinutes) /2))
+                    if (lastJob != null)
                     {
-                        new SendAlertMailTask
-                        {
-                            AlertSubject = string.Format("Error: Alert for work job service : {0} failure", job.JobInstanceName),
-                            Details = string.Format("In last 24 hours, invocation of {0} is only {1}, it's less than half of scheduled jobs",job.JobInstanceName, invocationCount),
-                            AlertName = string.Format("Error: Work job service {0}", job.JobInstanceName),
-                            Component = "work job service",
-                            Level = "Error"
-                        }.ExecuteCommand();
+                        lastCompleted = string.Format("{0} mins ago", Convert.ToInt32(DateTime.Now.Subtract(lastJob.completedAt).TotalMinutes));
+                    }
+                    else
+                    {
+                        lastCompleted = "N/A";
                     }
 
+                    jobDetail.Add(new WorkInstanceDetail(job.JobInstanceName, job.FrequencyInMinutes + "mins", lastCompleted, runtime.ToString() + "s", invocationCount.ToString(), faultCount.ToString(), faultRate, ErrorList));
+                    
+                    if (lastNhour == 1)
+                    {
+                        AlertThresholds thresholdValues = new JavaScriptSerializer().Deserialize<AlertThresholds>(ReportHelpers.Load(StorageAccount, "Configuration.AlertThresholds.json", ContainerName));
+                        string[] Igonored = new JavaScriptSerializer().Deserialize<string[]>(ReportHelpers.Load(StorageAccount, "Configuration.WorkerJobToBeIgnored.json", ContainerName));
+                        if (Igonored.Contains(job.JobInstanceName, StringComparer.OrdinalIgnoreCase)) continue;
+                        if (faultRate > thresholdValues.WorkJobErrorThreshold)
+                        {
+                            new SendAlertMailTask
+                            {
+                                AlertSubject = string.Format("Error: Alert for work job service : {0} failure", job.JobInstanceName),
+                                Details = string.Format("Rate of failure exceeded Error threshold for {0}. Threshold count : {1}%, failure in last 24 hour : {2}", job.JobInstanceName, thresholdValues.WorkJobErrorThreshold, faultCount),
+                                AlertName = string.Format("Error: Work job service {0}", job.JobInstanceName),
+                                Component = "work job service",
+                                Level = "Error"
+                            }.ExecuteCommand();
+                        }
+                        else if (faultRate > thresholdValues.WorkJobWarningThreshold)
+                        {
+                            new SendAlertMailTask
+                            {
+                                AlertSubject = string.Format("Warning: Alert for work job service: {0} failure", job.JobInstanceName),
+                                Details = string.Format("Rate of failure exceeded Warning threshold for {0}. Threshold count : {1}%, failure in last 24 hour : {2}", job.JobInstanceName, thresholdValues.WorkJobWarningThreshold, faultCount),
+                                AlertName = string.Format("Warning: Work job service {0}", job.JobInstanceName),
+                                Component = "work job service",
+                                Level = "Warning"
+                            }.ExecuteCommand();
+                        }
+
+                    }
+
+                    else
+                    {
+                        if (invocationCount < ((lastNhour * 60 / job.FrequencyInMinutes) / 2))
+                        {
+                            new SendAlertMailTask
+                            {
+                                AlertSubject = string.Format("Error: Alert for work job service : {0} failure", job.JobInstanceName),
+                                Details = string.Format("In last 24 hours, invocation of {0} is only {1}, it's less than half of scheduled jobs", job.JobInstanceName, invocationCount),
+                                AlertName = string.Format("Error: Work job service {0}", job.JobInstanceName),
+                                Component = "work job service",
+                                Level = "Error"
+                            }.ExecuteCommand();
+                        }
+                    }
                 }
             }
 
-             List<WorkServiceAdmin> allkey = new List<WorkServiceAdmin>();
-             allkey.Add(new WorkServiceAdmin(WorkServiceUserName, WorkServiceAdminKey));
-             allkey.Add(new WorkServiceAdmin(WorkServiceUserName,WorkServiceFailoverAdminKey));
+            if (lastNhour == 24)
+            {
+                var json = new JavaScriptSerializer().Serialize(jobDetail);
+                ReportHelpers.CreateBlob(StorageAccount, string.Format("WorkJobDetail{0:yyyy-MM-dd}.json", DateTime.Now), ContainerName, "application/json", ReportHelpers.ToStream(json));
+            }
+            else
+            {
+                var json = new JavaScriptSerializer().Serialize(jobDetail);
+                ReportHelpers.CreateBlob(StorageAccount, string.Format("WorkJobDetail{0}.json", lastNhour), ContainerName, "application/json", ReportHelpers.ToStream(json));
+            }
+
+            List<WorkServiceAdmin> allkey = new List<WorkServiceAdmin>();
+            allkey.Add(new WorkServiceAdmin(WorkServiceUserName, WorkServiceAdminKey));
+            allkey.Add(new WorkServiceAdmin(WorkServiceUserName,WorkServiceFailoverAdminKey));
+            var key = new JavaScriptSerializer().Serialize(allkey);
              
-             var json = new JavaScriptSerializer().Serialize(jobDetail);
-             var key = new JavaScriptSerializer().Serialize(allkey);
-             ReportHelpers.CreateBlob(StorageAccount, "WorkJobDetail.json", ContainerName, "application/json", ReportHelpers.ToStream(json));
-             ReportHelpers.CreateBlob(StorageAccount, "WorkServiceAdminKey.json", ContainerName, "application/json", ReportHelpers.ToStream(key));
+            ReportHelpers.CreateBlob(StorageAccount, "WorkServiceAdminKey.json", ContainerName, "application/json", ReportHelpers.ToStream(key));
         }
+
 
         private string getResultMessage(string message)
         {
